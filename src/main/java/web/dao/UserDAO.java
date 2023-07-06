42d25d6cba6da76d3f4fcfb9a8dda717ca5e96e3
Martianov Pavel <pall2006@bk.ru>
2023-07-06 14:32:47 +0300

    first commite

---
+++ src/main/java/web/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package web.Repository;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserRep {
+    List<User> getAllUsers();
+
+    void createUser(User user);
+
+    User readUser(long id);
+
+    void updateUser(User userOld, Long id);
+
+    void deleteUser(long id);
+}
---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,45 @@
+package web.Repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserRepImpl implements UserRep {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserRepImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+    @Override
+    public void createUser(User user) {
+        entityManager.persist(user);
+    }
+    @Override
+    public User readUser(long id) {
+        return entityManager.find(User.class, id);
+    }
+    @Override
+    public void updateUser(User userOld, Long id) {
+        User userNew = readUser(id);
+        userNew.setName(userOld.getName());
+        userNew.setLastName(userOld.getLastName());
+        userNew.setAge(userOld.getAge());
+        userNew.setNational(userOld.getNational());
+    }
+    @Override
+    public void deleteUser(long id) {
+        User userNow = entityManager.find(User.class, id);
+        entityManager.remove(userNow);
+    }
+}
